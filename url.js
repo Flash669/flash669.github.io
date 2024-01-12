5e34a66fe50eec4ec911e1d247a530bc348c7688
Flash669 <138119441+Flash669@users.noreply.github.com>
2024-01-12 10:56:31 -0300

    Delete url.js

--- url.js
+++
@@ -1,9 +0,0 @@
-
-  var enlace = document.querySelector('.btn.btn-md');
-
-  var dominio = 'https://television-libre.online';
-
-  var parteDinamica = enlace.getAttribute('href');
-
-  enlace.href = dominio + '/html/' + parteDinamica;
-
